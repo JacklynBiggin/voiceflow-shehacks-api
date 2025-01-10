075d9a04381e940f2d4bf0db1af312af65cc576d
Jacklyn Biggin <hi@jacklyn.dev>
2025-01-10 16:07:12 -0500

    Fix (again) for vercel + add more resillant error handling

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => res.send("You're not supposed to be here!"));
 
 // Helper Functions
@@ -23,43 +26,49 @@ app.post('/type', (req, res) => {
       return res.status(400).json({ error: 'All fields (name, value1, value2, emotion) are required.' });
   }
 
-  let points = 0
+  try {
+    let points = 0
 
-  // Handle name
-  namePoints = alphabetPosition(name)
-  namePoints.forEach( letterPosition => {
-    points += letterPosition
-  });
+    // Handle name
+    let namePoints = alphabetPosition(name)
+    namePoints.forEach( letterPosition => {
+      points += letterPosition
+    });
+  
+    // Handle custom values
+    points += `${value1}`.length
+    points += `${value2}`.length
+  
+    // Handle emotion
+    // We get the first letter of the emotion and convert it into a number
+    points += alphabetPosition(emotion)[0];
+  
+    var lastNumber = +points.toString().split('').pop();
+  
+    let dogTypes = [
+      "Poodle",
+      "German Sheppherd",
+      "Pug",
+      "Golden Retriever",
+      "Akita",
+      "Doge",
+      "Doberman",
+      "Boxer",
+      "Pitbull",
+      "Chiwawa",
+    ]
+  
+    // Catch all for people putting weird stuff as inputs for workshop purposes
+    if (isNaN(points)) {
+      return res.json({ type: "Fluffy", score: 1 });
+    }
 
-  // Handle custom values
-  points += `${value1}`.length
-  points += `${value2}`.length
+    // Return the specified JSON response
+    res.json({ type: dogTypes[lastNumber], score: points });
 
-  // Handle emotion
-  // We get the first letter of the emotion and convert it into a number
-  points += alphabetPosition(emotion)[0];
-
-  var lastNumber = +points.toString().split('').pop();
-
-  let dogTypes = [
-    "Poodle",
-    "German Sheppherd",
-    "Pug",
-    "Golden Retriever",
-    "Akita",
-    "Doge",
-    "Doberman",
-    "Boxer",
-    "Pitbull",
-    "Chiwawa",
-  ]
-
-
-  // Return the specified JSON response
-  res.json({ type: dogTypes[lastNumber], score: points });
+  } catch (error) {  
+    res.status(500).json({ error: 'An error occurred.' });
+  }
 });
-
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
